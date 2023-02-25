260a9dc303771e73f43913d368563c3090033daf
jl <jeanlouis.macle@gmail.com>
2023-02-25 14:56:34 -0600

    Batch files to the Cypress specs to use by the ProcessBuilder.

---
+++ e2e_cypress-batch-files/US2.1.a-Chrome-Level-easy-at-startup.bat
@@ -0,0 +1,3 @@
+@REM This script is meant to be run by a process started by Java
+@REM and will fail if run directly from a command prompt.
+npx cypress run -b chrome --config screenshotOnRunFailure=false,video=false --spec "./cypress/e2e/US2.1.a-Chrome-Level-easy-at-startup.cy.js"
