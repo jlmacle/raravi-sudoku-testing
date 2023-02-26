a0dc2183fe6c8c787d0d79850f91a17b3348b203
jl <jeanlouis.macle@gmail.com>
2023-02-26 15:17:28 -0600

    Files renamed

--- cypress/e2e/US2.1-Level-easy-at-startup.cy.js
+++ cypress/e2e/US2.1-Level-easy-at-startup.cy.js
@@ -1,4 +1,4 @@
-describe('US 2.1.a - Chrome - Level easy at startup', () => {
+describe('US 2.1 - Level easy at startup', () => {
   it('passes', () => {
     cy.visit('/')
     cy.get('.status__difficulty-select').should('have.value', 'Easy')
