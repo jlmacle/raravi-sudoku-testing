97ed864ecb7a23232aae09dd01b9fec35f36576e
jl <jeanlouis.macle@gmail.com>
2023-02-26 14:27:40 -0600

    Cypress spec for user story 2.1.a

---
+++ cypress/e2e/scripts/US2.1.a-Chrome-Level-easy-at-startup.bat
@@ -0,0 +1,3 @@
+cd ../../../
+call npx cypress run -b chrome --config screenshotOnRunFailure=false,video=false --spec "./cypress/e2e/US2.1.a-Chrome-Level-easy-at-startup.cy.js"
+cd cypress/e2e/scripts
