a93726cb57bd9eb946d617b8c67c71d412c95bde
jl <jeanlouis.macle@gmail.com>
2023-02-27 08:45:41 -0600

    Additions to utility class

--- src/main/java/testing/util/Ext.java
+++ src/main/java/testing/util/Ext.java
@@ -24,6 +24,8 @@ public class Ext
     public static final Map<String,String> US2_1 = new HashMap<>();
     public static final String US2_1_A_CHROME = "US2.1.a-Chrome-Level-easy-at-startup.bat";
     public static final String US2_1_B_EDGE = "US2.1.b-Edge-Level-easy-at-startup.bat";
+    public static final String US2_2_1_A = "US2.2-1.a-Edge-Visibility-of-level-medium.bat";
+    public static final String US2_2_2_B = "US2.2-2.a-Firefox-Visibility-of-level-hard.bat";
     public static final String THE_BROWSER_IS_ON_THE_HOME_PAGE = "The browser is on the home page";
     public static final String THE_DEFAULT_LEVEL_IS_EASY = "The default level is \"Easy\"";
     
