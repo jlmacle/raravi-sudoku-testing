2db679473cf6ab14529c4bf8486ec5eee3436734
jl <jeanlouis.macle@gmail.com>
2023-02-27 14:14:11 -0600

    Step definitionss for scenario US 2.2.1 with Edge

--- src/test/java/testing/StepDefinitions.java
+++ src/test/java/testing/StepDefinitions.java
@@ -21,10 +21,7 @@ public class StepDefinitions {
 // US 2.1
 @When("{string} is on the homepage")
     public void is_on_the_homepage(String browser) {
-        System.out.println("browser: "+browser);
         String batchFileName = Ext.US2_1.get(browser);
-        System.out.println("batchFileName: "+batchFileName);
-        System.out.println(Ext.US2_1);
         boolean containsBrowserName = batchFileName.contains(browser);
         String cypressScriptContent = CypressSpec.getCypressSpecContent(batchFileName);
         boolean oneInstanceOfVisitAndOnHomePage = (cypressScriptContent.indexOf(Ext.HOME_PAGE_VISIT_PATTERN)==cypressScriptContent.lastIndexOf(Ext.HOME_PAGE_VISIT_PATTERN));
@@ -36,15 +33,17 @@ public class StepDefinitions {
         assertTrue(CypressSpec.passed(Ext.US2_1.get(browser))); 
     }
 
-    // @Then("{string} should be visible, selectable and Easy should not be visible anymore")
-    // public void should_be_visible_selectable_and_Easy_should_not_be_visible_anymore(String s) {
-    //     // Write code here that turns the phrase above into concrete actions
-    // }
+    @When("{string} is on the homepage, and I click on the level {string}")
+    public void is_on_the_homepage_and_I_click_on_the_level(String browserName, String selectedLevel) {
+        String batchFileName = Ext.US2_1.get(browserName);
+    }
 
-    // @When("I click on the level {string}")
-    // public void I_click_on_the_level(String s) {
-    //     // Write code here that turns the phrase above into concrete actions
-    // }
+    @Then("{string} should be visible, selectable and Easy should not be visible anymore")
+    public void should_be_visible_selectable_and_Easy_should_not_be_visible_anymore(String s) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    
 
     
 
