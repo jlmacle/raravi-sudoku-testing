3ae4009890e32716898e98fd3dc237bf12a870c0
jl <jeanlouis.macle@gmail.com>
2023-02-27 13:09:25 -0600

    Modified to take into account the new Levels folder

--- src/main/java/testing/util/Ext.java
+++ src/main/java/testing/util/Ext.java
@@ -22,10 +22,11 @@ public class Ext
 
     //US 2.1:
     public static final Map<String,String> US2_1 = new HashMap<>();
-    public static final String US2_1_A_CHROME = "US2.1.a-Chrome-Level-easy-at-startup.bat";
-    public static final String US2_1_B_EDGE = "US2.1.b-Edge-Level-easy-at-startup.bat";
-    public static final String US2_2_1_A_EDGE = "US2.2-1.a-Edge-Visibility-of-level-medium.bat";
-    public static final String US2_2_2_B = "US2.2-2.a-Firefox-Visibility-of-level-hard.bat";
+    public static final String LEVELS_SCRIPT_FOLDER = "Levels/";
+    public static final String US2_1_A_CHROME = LEVELS_SCRIPT_FOLDER+"US2.1.a-Chrome-Level-easy-at-startup.bat";
+    public static final String US2_1_B_EDGE = LEVELS_SCRIPT_FOLDER+"US2.1.b-Edge-Level-easy-at-startup.bat";
+    public static final String US2_2_1_A_EDGE = LEVELS_SCRIPT_FOLDER+"US2.2-1.a-Edge-Visibility-of-level-medium.bat";
+    public static final String US2_2_2_B = LEVELS_SCRIPT_FOLDER+"US2.2-2.a-Firefox-Visibility-of-level-hard.bat";
     public static final String THE_BROWSER_IS_ON_THE_HOME_PAGE = "The browser is on the home page";
     public static final String THE_DEFAULT_LEVEL_IS_EASY = "The default level is \"Easy\"";
     
