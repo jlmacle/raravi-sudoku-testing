b71451085f704743a68e3e6a5089b8d5c44748ec
jl <jeanlouis.macle@gmail.com>
2023-02-27 08:14:38 -0600

    Code modified

--- cypress/e2e/US2.2-1-Visibility-of-level-medium.cy.js
+++ cypress/e2e/US2.2-1-Visibility-of-level-medium.cy.js
@@ -1,4 +1,4 @@
-describe('US 2.2 - Access to the level Medium', () => {
+describe('US 2.2 - Access to level Medium can be found', () => {
   it('passes', () => {
     cy.visit('/')
     // When clicking on Easy, "Medium" should be visible, selectable and "Medium" should be visible instead of Easy
