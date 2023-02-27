1ed1171dd5bfde033293a916c8e9c5326fe6ab1d
jl <jeanlouis.macle@gmail.com>
2023-02-27 13:28:09 -0600

    Files in renamed folders

--- cypress/e2e/1.Levels/US2.1-Visibility-of-level-easy-at-startup.cy.js
+++
@@ -1,6 +0,0 @@
-describe('US 2.1 - Level easy at startup', () => {
-  it('passes', () => {
-    cy.visit('/')
-    cy.get('.status__difficulty-select').should('have.value', 'Easy')
-  })
-})
--- e2e_cypress-batch-files/1.Levels/US2.2-1.a-Edge-Visibility-of-level-medium.bat
+++
@@ -1,3 +0,0 @@
-@REM This script is meant to be run by a process started by Java
-@REM and will fail if run directly from a command prompt.
-npx cypress run -b edge --config screenshotOnRunFailure=false,video=false --spec "./cypress/e2e/US2.2-1-Visibility-of-level-medium.cy.js"
