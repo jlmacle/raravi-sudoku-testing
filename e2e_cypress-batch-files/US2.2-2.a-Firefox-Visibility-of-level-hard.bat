c2c27ae3d2e135344665ea5d6bc7434a42b824b1
jl <jeanlouis.macle@gmail.com>
2023-02-27 08:43:29 -0600

    Script useful to test US2.2.2

--- e2e_cypress-batch-files/US2.2-2.a-Firefox-Visibility-of-level-hard.bat
+++ e2e_cypress-batch-files/US2.2-2.a-Firefox-Visibility-of-level-hard.bat
@@ -1,3 +1,3 @@
 @REM This script is meant to be run by a process started by Java
 @REM and will fail if run directly from a command prompt.
-npx cypress run -b firefox --config screenshotOnRunFailure=false,video=false --spec "./cypress/e2e/US2.2-1-Visibility-of-level-medium.cy.js"
+npx cypress run -b firefox --config screenshotOnRunFailure=false,video=false --spec "./cypress/e2e/US2.2-2-Visibility-of-level-hard.cy.js"
