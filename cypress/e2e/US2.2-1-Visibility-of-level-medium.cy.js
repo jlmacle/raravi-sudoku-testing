6426394c72d8e4e2501c88f7e1d77b32cabf49e8
jl <jeanlouis.macle@gmail.com>
2023-02-27 12:50:24 -0600

    Comment modification

--- cypress/e2e/US2.2-1-Visibility-of-level-medium.cy.js
+++ cypress/e2e/US2.2-1-Visibility-of-level-medium.cy.js
@@ -1,7 +1,7 @@
 describe('US 2.2 - Access to level Medium can be found', () => {
   it('passes', () => {
     cy.visit('/')
-    // When clicking on Easy, "Medium" should be visible, selectable and "Medium" should be visible instead of Easy
+    // When clicking on "Easy", "Medium" should be visible, selectable and "Easy" should not be visible anymore
     cy.get('.status__difficulty-select').select('Medium')
     cy.contains('Medium')
       .should('not.have.value', 'Easy')
